b228903edbd248ef166f95b23198207ba6f9b4c8
Jinkange <chrre13@naver.com>
2022-11-01 17:38:31 +0900

    component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Footer from "./component/footer/Footer";
+import Herder from "./component/header/Herder";
+import Main from "./component/main/Main";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <App />
